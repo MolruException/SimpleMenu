e6dee87b8dc74c6da179069f97e62ca163187be3
MolruException <molruexception@gmail.com>
2023-04-01 10:59:39 +0900

    Chore: 프로젝트 초기화

---
+++ src/main/java/com/molruexception/simplemenu/SimpleMenu.java
@@ -0,0 +1,17 @@
+package com.molruexception.simplemenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleMenu extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
